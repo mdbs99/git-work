fdc55078a570afb249e0dd5c4f32ef3f30f59a29
Fabricio Cabral <fabriciofx@gmail.com>
2017-08-04 00:23:05 -0300

    #1: Polish the code

--- git-work.sh
+++ git-work.sh
@@ -1,77 +1,82 @@
+#!/bin/sh
+#
 # git-work
 #
-#MIT License
+# MIT License
 #
-#Copyright (c) 2017 Marcos Douglas B. Santos
+# Copyright (c) 2017 Marcos Douglas B. Santos
 #
-#Permission is hereby granted, free of #charge, to any person obtaining a copy
-#of this software and associated documentation files (the "Software"), to deal
-#in the Software without restriction, including without limitation the rights
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-#copies of the Software, and to permit persons to whom the Software is
-#furnished to do so, subject to the following conditions:
+# Permission is hereby granted, free of #charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
 #
-#The above copyright notice and this permission notice shall be included in all
-#copies or substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
 #
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-#SOFTWARE.
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
 set -e
 
-help() {
-    echo "Usage: git work issue 123"
-    echo "       git work commit \"message\""
-    echo "       git work done 123"
-    echo "       git work push origin master"
-    echo "       git work release 1.0 \"message\" "
-    echo "       git work pr 321"
+usage() {
+  echo -e "Usage: git work <subcommand>\n"
+  echo "Avaliable subcommands are:"
+  echo "    issue <id>                Start a new branch from issue <id>"
+  echo "    commit <message>          Commit current work using <message>"
+  echo "    done <id>                 Finish current work and merge"
+  echo "    push                      Push to the server"
+  echo "    release <tag> <message>   Release a new tag called <tag> with <message>"
+  echo "    pr <id>                   Create a branch from a pull-request"
 }
 
 if [ -z $1 ] || [ -z $2 ]; then
-  help
-  exit 0
+  usage
+  exit 1
 fi
 
-# start a new branch
-if [ $1 = "issue" ]; then
-  git checkout -b "$2"
-fi
-
-# commit current work
-if [ $1 = "commit" ]; then
-  git commit -am $2
-fi
-
-# finish current work and merge
-if [ $1 = "done" ]; then
-  git checkout master
-  git merge $2
-fi
-
-# push to the server
-if [ $1 = "push" ]; then
-  git push $2 $3
-fi
-
-# release a new tag
-if [ $1 = "release" ]; then
-  git checkout master
-  if [ -z $3 ]; then
-    git tag $2
-  else
-    git tag $2 -m $3
-  fi 
-  git push --tags
-fi
-
-# create a branch from a pull-request
-if [ $1 = "pr" ]; then
-  git fetch $1 pull/$2/head:pr/$2
-  git checkout pr/$2
-fi
+case "$1" in
+  # start a new branch
+  issue)
+    git checkout -b "$2"
+    ;;
+  # commit current work
+  commit)
+    git commit -am "$2"
+    ;;
+  # finish current work and merge
+  done)
+    git checkout master
+    git merge "$2"
+    ;;
+  # push to the server
+  push)
+    git push origin "$2"
+    ;;
+  # release a new tag
+  release)
+    git checkout master
+    if [ -z "$3" ]; then
+      git tag "$2"
+    else
+      git tag "$2" -m "$3"
+    fi
+    git push --tags
+    ;;
+  # create a branch from a pull-request
+  pr)
+    git fetch origin pull/"$2"/head:pr/"$2"
+    git checkout pr/"$2"
+    ;;
+  *)
+    usage
+    exit 1
+    ;;
+esac
